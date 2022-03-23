331900a37356c54a8a65355bc7533ec5d7290ee9
Beliz Pehlivan <belizpehlivan@gmail.com>
2022-03-23 18:41:58 +0300

    if-else

---
+++ if-else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // bool condition = true;
+
+            // if (condition)
+            // {
+            //     Console.WriteLine("Değişken: true");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Değişken: false");
+            // }
+            int time = DateTime.Now.Hour;
+            string message = time>=6 && time <11 ? "Good Morning!" : time<=18 ? "Good Afternoon!" : "Good evening!";
+            Console.WriteLine(message);
+        }
+    }
+}
