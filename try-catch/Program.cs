2f084726b9010708c32715cd022a9732fb7ec53c
Beliz Pehlivan <belizpehlivan@gmail.com>
2022-03-23 17:37:58 +0300

    try-catch

---
+++ try-catch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                Console.WriteLine("Enter a num");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Entered number:"  + num1);
+            }
+            catch(Exception ex){
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+            // finally{
+            //     Console.WriteLine("Completed");
+            // }
+            
+
+            try{
+                //int x = int.Parse(null);
+                //int y = int.Parse("test");
+                int z = int.Parse("-200000000000");
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Null value entered");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("Completed");
+            }
+        }
+    }
+}
