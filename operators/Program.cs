7059ccfcdc31019dd4044f1dda380b2f39ce780e
Beliz Pehlivan <belizpehlivan@gmail.com>
2022-03-23 14:30:21 +0300

    operators

---
+++ operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            // Mantıksal || && !
+            // İlişkisel < > <= >= !=
+
+            int num1 = 10;
+            int num2 = 2;
+            Console.WriteLine((num1++)*num2);
+            Console.WriteLine(num1/num2);
+            Console.WriteLine(num1+num2);
+            Console.WriteLine(num1-num2);
+            Console.WriteLine(num1%num2);
+
+        }
+    }
+}
